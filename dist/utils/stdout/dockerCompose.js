ff2650c4933de141c477387bd1fb7d30de76e871
Oleg Chepelev <yorkblansh@yandex.com>
2023-03-29 16:22:55 +0300

    p

--- dist/utils/stdout/dockerCompose.js
+++ dist/utils/stdout/dockerCompose.js
@@ -2,7 +2,9 @@ export const containersBuildStepList = (data) => {
     // example of regexp below: [ `#12 [image-name 4/6]` ]
     const matchList = data.match(/#\d{1,} \[\S+ \d{1,}\/\d{1,}\]/gm);
     if (matchList) {
-        return matchList.map((match) => {
+        return matchList
+            .map((match) => match.replace(/\[/g, '').replace(/\]/g, ''))
+            .map((match) => {
             const matchElements = match.split(' ');
             if (matchElements.length === 3) {
                 return matchElements.map((v, i) => ({
--- src/utils/stdout/dockerCompose.ts
+++ src/utils/stdout/dockerCompose.ts
@@ -3,25 +3,27 @@ export const containersBuildStepList = (data: string) => {
 	const matchList = data.match(/#\d{1,} \[\S+ \d{1,}\/\d{1,}\]/gm)
 
 	if (matchList) {
-		return matchList.map((match) => {
-			const matchElements = match.split(' ')
+		return matchList
+			.map((match) => match.replace(/\[/g, '').replace(/\]/g, ''))
+			.map((match) => {
+				const matchElements = match.split(' ')
 
-			if (matchElements.length === 3) {
-				return matchElements.map((v, i) => ({
-					globalStep: matchElements[0],
-					imageName: matchElements[1],
-					currentImageSteps: matchElements[2]
-						.trim()
-						.split('/')
-						.map((v, i, stepValues) => {
-							if (stepValues.length === 2)
-								return {
-									currentStep: stepValues[0],
-									totalSteps: stepValues[1],
-								}
-						})[0],
-				}))
-			}
-		})
+				if (matchElements.length === 3) {
+					return matchElements.map((v, i) => ({
+						globalStep: matchElements[0],
+						imageName: matchElements[1],
+						currentImageSteps: matchElements[2]
+							.trim()
+							.split('/')
+							.map((v, i, stepValues) => {
+								if (stepValues.length === 2)
+									return {
+										currentStep: stepValues[0],
+										totalSteps: stepValues[1],
+									}
+							})[0],
+					}))
+				}
+			})
 	}
 }
