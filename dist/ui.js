6eac28b2dbb6d3c48519d5ec7564027fbbce9037
Oleg Chepelev <yorkblansh@yandex.com>
2023-03-30 13:06:13 +0300

    p

--- dist/ui.js
+++ dist/ui.js
@@ -42,7 +42,7 @@ export const App = () => {
             onSelect: (item) => {
                 setSelectInputFocus(false);
                 commandExecutor(item.value, (cbProps) => {
-                    console.log({ cbProps });
+                    console.log({ exitCode: cbProps.dockerComposeExitCode });
                     if (cbProps.dockerComposeExitCode) {
                         setIsDone(cbProps.dockerComposeExitCode === 0 ? true : false);
                         console.log('HERE MUST BE INPUT FoCUS TRUEEE!!!!');
