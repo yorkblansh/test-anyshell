04a923d94631c5a147274fca6776cc02ee1e40e6
Oleg Chepelev <yorkblansh@yandex.com>
2023-03-31 17:42:07 +0300

    add www1

--- src/ui.tsx
+++ src/ui.tsx
@@ -14,6 +14,7 @@ import { commandExecutor } from './utils/commandExecutor.js'
 import { useProcessResultReset } from './hooks/useProcessResultsReset.js'
 
 //www
+//www1
 export const App = () => {
 	useBeforeRender(() => {
 		shell.exec('clear')
