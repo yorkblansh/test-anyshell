24967c5f6853ab103dfe8b5e47ab8e2e8d55394d
Oleg Chepelev <yorkblansh@yandex.com>
2023-03-31 18:10:29 +0300

    1111

--- src/ui.tsx
+++ src/ui.tsx
@@ -14,6 +14,7 @@ import { commandExecutor } from './utils/commandExecutor.js'
 import { useProcessResultReset } from './hooks/useProcessResultsReset.js'
 
 //11
+//1111
 export const App = () => {
 	useBeforeRender(() => {
 		shell.exec('clear')
