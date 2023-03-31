88658361b1964bdd3ce0533cef977c83c93cc75e
Oleg Chepelev <yorkblansh@yandex.com>
2023-03-31 18:00:01 +0300

    del2

--- src/ui.tsx
+++ src/ui.tsx
@@ -14,7 +14,6 @@ import { commandExecutor } from './utils/commandExecutor.js'
 import { useProcessResultReset } from './hooks/useProcessResultsReset.js'
 
 //comment from dev-feat
-//one more comment from dev-feat
 export const App = () => {
 	useBeforeRender(() => {
 		shell.exec('clear')
